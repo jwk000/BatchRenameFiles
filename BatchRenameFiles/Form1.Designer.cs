d6e9af2caac354114b2019f7158852399841e775
jwk <j__w__k@163.com>
2018-04-09 23:26:30 +0800

    Add project files.

---
+++ BatchRenameFiles/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace BatchRenameFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dirview = new System.Windows.Forms.ListView();
+            this.txtFind = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtReplace = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblret = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dirview
+            // 
+            this.dirview.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dirview.Location = new System.Drawing.Point(0, 82);
+            this.dirview.Name = "dirview";
+            this.dirview.Size = new System.Drawing.Size(492, 301);
+            this.dirview.TabIndex = 0;
+            this.dirview.UseCompatibleStateImageBehavior = false;
+            this.dirview.View = System.Windows.Forms.View.List;
+            // 
+            // txtFind
+            // 
+            this.txtFind.Location = new System.Drawing.Point(178, 28);
+            this.txtFind.Name = "txtFind";
+            this.txtFind.Size = new System.Drawing.Size(151, 21);
+            this.txtFind.TabIndex = 1;
+            this.txtFind.TextChanged += new System.EventHandler(this.txtFind_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(107, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "查找文字：";
+            // 
+            // txtReplace
+            // 
+            this.txtReplace.Location = new System.Drawing.Point(178, 55);
+            this.txtReplace.Name = "txtReplace";
+            this.txtReplace.Size = new System.Drawing.Size(151, 21);
+            this.txtReplace.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(107, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "替换文字：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 50);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "重新选择 一个目录";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button2.Location = new System.Drawing.Point(406, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 50);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "点我替换";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblret
+            // 
+            this.lblret.AutoSize = true;
+            this.lblret.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblret.Location = new System.Drawing.Point(0, 0);
+            this.lblret.Name = "lblret";
+            this.lblret.Size = new System.Drawing.Size(0, 12);
+            this.lblret.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(335, 28);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(55, 48);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 383);
+            this.Controls.Add(this.lblret);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtReplace);
+            this.Controls.Add(this.txtFind);
+            this.Controls.Add(this.dirview);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView dirview;
+        private System.Windows.Forms.TextBox txtFind;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtReplace;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblret;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ BatchRenameFiles/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BatchRenameFiles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = Environment.CurrentDirectory;
+            dlg = new FolderBrowserDialog();
+            dlg.SelectedPath = Environment.CurrentDirectory;
+            dlg.ShowNewFolderButton = true;
+            button2.Enabled = false;
+
+            showCurDir();
+        }
+        FolderBrowserDialog dlg;
+        Dictionary<string, string> dirFiles = new Dictionary<string, string>();
+
+        
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            this.dirview.Height = this.ClientSize.Height - 82;
+        }
+
+        void showCurDir()
+        {
+            this.Text = dlg.SelectedPath;
+
+            var files = Directory.GetFiles(dlg.SelectedPath);
+            dirFiles.Clear();
+            this.dirview.Clear();
+            this.dirview.BeginUpdate();
+            foreach (var file in files)
+            {
+                dirFiles.Add(Path.GetFileName(file), file);
+                this.dirview.Items.Add(Path.GetFileName(file));
+            }
+            this.dirview.EndUpdate();
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //打开目录
+
+            var ret = dlg.ShowDialog();
+            if (ret == DialogResult.OK)
+            {
+                showCurDir();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //必须有查找字符串
+            if (string.IsNullOrEmpty(txtFind.Text))
+            {
+                MessageBox.Show("输入您需要替换的文字！");
+                return;
+            }
+
+            //去重
+            if(txtFind.Text== txtReplace.Text)
+            {
+                MessageBox.Show("为什么查找和替换的文字一样。。");
+                return;
+            }
+
+            List<string> reps = new List<string>();
+            foreach (var file in dirFiles.Keys)
+            {
+                if (file.IndexOf(txtFind.Text) >= 0)
+                {
+                    reps.Add(file);
+                }
+            }
+
+            //替换
+            dirview.Items.Clear();
+            dirview.BeginUpdate();
+
+            foreach(var file in reps)
+            {
+                string newfilename = file.Replace(txtFind.Text, txtReplace.Text);
+                dirview.Items.Add(newfilename);
+
+                string oldfilepath = dirFiles[file];
+                string newfilepath = Path.GetDirectoryName(oldfilepath)+"\\" + newfilename;
+                dirFiles.Add(newfilename, newfilepath);
+                dirFiles.Remove(file);
+
+                //改名
+                File.Move(oldfilepath, newfilepath);
+            }
+
+            dirview.EndUpdate();
+            
+            lblret.Text = string.Format("成功替换了{0}个文件名", reps.Count);
+            lblret.ForeColor = Color.Red;
+
+            txtFind.Text = "";
+            
+        }
+
+        private void txtFind_TextChanged(object sender, EventArgs e)
+        {
+            if (txtFind.Text.Length > 0)
+            {
+                button2.Enabled = true;
+            }
+            else
+            {
+                button2.Enabled = false;
+            }
+            dirview.Items.Clear();
+            dirview.BeginUpdate();
+            //根据查找内容过滤文件名
+            foreach(var file in dirFiles.Keys)
+            {
+                if (file.IndexOf(txtFind.Text) >= 0 || txtFind.Text=="")
+                {
+                    dirview.Items.Add(file);
+                }
+            }
+            dirview.EndUpdate();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var s = txtFind.Text;
+            txtFind.Text = txtReplace.Text;
+            txtReplace.Text = s;
+        }
+    }
+}
